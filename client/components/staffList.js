ce82b035606d84c859f7a1c15395458f2d8d2ad2
taHo <hoseininejad@gmail.com>
2017-08-22 12:56:25 +0200

    Fixed bug.

--- client/components/staffList.js
+++ client/components/staffList.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {createContainer} from 'react-meteor-data';
+import {Staffs} from '../../imports/collections/staffsCol';
 
 
-const StaffList = () =>{
-    return(
+const StaffList = (props) => {
+    console.log(props);
+    return (
         <div>
-            from the List!
+            {props.staffs.map((element) => element.name)}
         </div>
     );
 };
 
-export default StaffList;
+export default createContainer(() => {
+    Meteor.subscribe('staffsCol', 20);
+    return {staffs:Staffs.find({}).fetch()};
+}, StaffList);
--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ const App = () => {
     return(
         <div>
             hello there!
-            < StaffList/>
+            <StaffList/>
         </div>
     );
 };
